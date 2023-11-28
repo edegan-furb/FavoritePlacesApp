d31bc2e02a6652fb96fbd15671b77596742a7222
edegan <eduardord324@gmail.com>
2023-11-28 17:01:29 -0300

    Class 199 - 201

--- components/Places/ImagePicker.js
+++ components/Places/ImagePicker.js
@@ -1,19 +1,25 @@
-import { Alert, Button, View } from "react-native";
+import { Alert, Image, StyleSheet, Text, View } from "react-native";
 import {
   launchCameraAsync,
   useCameraPermissions,
   PermissionStatus,
 } from "expo-image-picker";
+import { useState } from "react";
+
+import { Colors } from "../../constants/colors";
+import OutlinedButton from "../UI/OutlinedButton";
 
 function ImagePicker() {
+  const [pickedImage, setPickedImage] = useState();
+
   const [cameraPermissionInformation, requestPermission] =
     useCameraPermissions();
 
   async function verifyPermissions() {
     if (cameraPermissionInformation.status === PermissionStatus.UNDETERMINED) {
-      const PermissionResponse = await requestPermission();
+      const permissionResponse = await requestPermission();
 
-      return PermissionResponse.granted;
+      return permissionResponse.granted;
     }
 
     if (cameraPermissionInformation.status === PermissionStatus.DENIED) {
@@ -23,6 +29,7 @@ function ImagePicker() {
       );
       return false;
     }
+
     return true;
   }
 
@@ -32,20 +39,47 @@ function ImagePicker() {
     if (!hasPermission) {
       return;
     }
+
     const image = await launchCameraAsync({
       allowsEditing: true,
       aspect: [16, 9],
       quality: 0.5,
     });
-    console.log(image);
+    if (!image.canceled) {
+      setPickedImage(image.assets[0].uri);
+    }
+  }
+
+  let imagePreview = <Text>No image taken yet.</Text>;
+
+  if (pickedImage) {
+    imagePreview = <Image style={styles.image} source={{ uri: pickedImage }} />;
   }
 
   return (
     <View>
-      <View></View>
-      <Button title="Take Image" onPress={takeImageHandler} />
+      <View style={styles.imagePreview}>{imagePreview}</View>
+      <OutlinedButton icon={"camera"} onPress={takeImageHandler}>
+        Take Image
+      </OutlinedButton>
     </View>
   );
 }
 
 export default ImagePicker;
+
+const styles = StyleSheet.create({
+  imagePreview: {
+    width: "100%",
+    height: 200,
+    marginVertical: 8,
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: Colors.primary100,
+    borderRadius: 4,
+  },
+  image: {
+    width: "100%",
+    height: "100%",
+  },
+});
